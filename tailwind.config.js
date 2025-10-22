7f2273bcb1782fea0b2cb972eca4d32acb2cef19
foxa5002-cell <foxa5002@gmail.com>
2025-10-22 21:58:52 +0700

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          600: '#6A1B9A',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
